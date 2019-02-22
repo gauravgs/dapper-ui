b0a1f8d6349282f1a6920b621db41baa4f4c5de8
Gaurav Sharma <gauravngp7@gmail.com>
2019-02-22 19:11:40 +0530

    v6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Sample from "./Trials/Sample";
-import TopNav from "./Trials/TopNav";
-import LandingGrid from "./Trials/LandingGrid";
-import CreateMultiSigInstitute from "./Trials/CreateMultiSigInstitute";
-import CreateMultiSigStudent from "./Trials/CreateMultiSigStudent";
-import Search from "./Trials/Search.jsx";
-import Profile from "./Trials/Profile";
-import Popup from "./Trials/Popup";
-import RequestCard from "./Trials/RequestCard";
-import ReqRunner from "./Trials/ReqRunner";
-import DashboardC from "./Trials/DashboardC";
-import OwnershipReq from "./Trials/OwnershipReq";
-import MyProfile from "./Trials/MyProfile";
-import InstProfile from "./Trials/InstProfile";
-import DrawerRHS from "./Trials/DrawerRHS";
-import SimpleTabs from "./Trials/SimpleTabs";
-import SignUpButton from "./Trials/SignUpButton";
-import UploadPage from "./Trials/UploadPage";
-import LHS from "./Trials/LHS";
+import ClippedDrawer from "./Trials/ClippedDrawer";
 
 class App extends Component {
   constructor(props) {
@@ -27,7 +7,11 @@ class App extends Component {
     this.state = {};
   }
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <ClippedDrawer />
+      </div>
+    );
   }
 }
 
